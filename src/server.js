18771ef99aaf41b59dccac22075d0259a29644a1
Romain Fernandez <rfernandez@ooti.co>
2023-03-30 12:09:43 +0200

    test again

--- src/server.js
+++ src/server.js
@@ -60,12 +60,12 @@ if (config.help) {
 }
 else if (config.http || config.https) {
     const app = express();
-    // app.get("/", (req, res) => {
-    //   res.send("Express on Vercel");
-    // });
-    // app.listen(5000, () => {
-    //   console.log("Running on port 5000.");
-    // });
+    app.get("/", (req, res) => {
+      res.send("Express on Vercel");
+    });
+    app.listen(5000, () => {
+      console.log("Running on port 5000.");
+    });
 
     const webServer = new WebServer(config, app);
 
