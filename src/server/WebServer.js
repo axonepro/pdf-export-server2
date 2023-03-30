dd9917599673ad97ea604e37d7e16b0e244d7cc5
Romain Fernandez <rfernandez@ooti.co>
2023-03-30 11:14:44 +0200

    try change port

--- src/server/WebServer.js
+++ src/server/WebServer.js
@@ -238,7 +238,7 @@ module.exports = class WebServer extends ExportServer {
                 //     console.log('Https server started on port ' + this.httpsPort);
                 //     resolve();
                 // });
-                this.httpsServer.listen(process.env.PORT, () => {
+                this.httpsServer.listen(8080, () => {
                     console.log('Https server started on port ' + process.env.PORT);
                     resolve();
                 });
